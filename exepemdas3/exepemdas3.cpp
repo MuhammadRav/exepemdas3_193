ea2b5913abfe0a4dc271e430e8e12ac6b4e35569
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-12 15:00:31 +0700

    a

--- exepemdas3/exepemdas3.cpp
+++ exepemdas3/exepemdas3.cpp
@@ -1,20 +1,81 @@
-// exepemdas3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class bidangDatar {
+private:
+    int x; //variabel untuk menyimpan input dari lingkaran maupun bujursangkar
+public:
+    bidangDatar(){ //constructor
+        x = 0;
+    }
+    virtual void input(){} // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+    virtual float Luas(int a){return 0;} //fungsi untuk menghitung luas     
+    virtual float Keliling(int a){return 0;} //fungsi untuk menghitung keliling
+    void setX(int a) { //fungsi untuk memberi/mengirim nilai pada x
+        this->x = a;
+    }
+    int getX() { //fungsi untuk membaca/mengambil nilai dalam x
+        return x;
+    }
+};
+
+class Lingkaran :public bidangDatar {
+    /*lengkapi disini*/
+    
+public:
+    int x;
+    void input() {
+        float a;
+        cout << "\nLingkaran dibuat\n";
+        cout << "Masukkan jejari: ";
+        cin >> a;
+    }
+    float luas(float a) {
+        return 3.14 * a * a;
+        
+    }
+    float keliling(float a) {
+        return 2 * 3.14 * a;
+    }
+    void setX(int a) {
+      
+    }
+};
+
+
+class Bujursangkar :public bidangDatar {
+    /*lengkapi disini*/ 
+public:
+    int x;
+
+    void input() {
+        float a;
+        cout << "\nBujursangkar dibuat\n";
+        cout << "Masukkan sisi: ";
+        cin >> a;
+    }
+    float luas(float a) {
+        return a * a;
+    }
+    float keliling(float a) {
+        return 4 * a;
+    }
+    void setX(int a) {
+     
+    }
+};
+
+
+int main() {
+    /*lengkapi disini*/ 
+
+    bidangDatar* obyek;
+    Lingkaran a;
+    Bujursangkar b;
+
+    obyek = &a;
+    obyek->input();
+    obyek = &b;
+    obyek->input();
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
